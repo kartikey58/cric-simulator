b154e8b53128303bdb87c12143c889db52cb7e0c
Kartikey Krishna <kartikeykrishn7861@gmail.com>
2026-08-27 04:11:33 +0530

    Correct wheel sector labels rotation to eliminate result mismatch

--- src/components/MatchSetup.jsx
+++ src/components/MatchSetup.jsx
@@ -936,7 +936,7 @@ export default function MatchSetup({ onStartMatch }) {
                       position: 'absolute',
                       top: 0, left: 0,
                       width: '100%', height: '100%',
-                      transform: `rotate(${i * 60 + 90}deg)`,
+                      transform: `rotate(${i * 60 + 30}deg)`,
                       transformOrigin: '50% 50%',
                       display: 'flex',
                       justifyContent: 'center',
